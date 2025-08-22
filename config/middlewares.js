f373d6e29df16b7e6bae5b522fcd17b2e4e96b3e
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2025-08-22 11:26:34 +0400

    fix: configure CSP for Strapi Cloud image support
    - Add Content Security Policy configuration to allow images from Strapi Cloud storage
    - Allow img-src and media-src from storage.googleapis.com and *.strapiapp.com domains
    - Fix 400 bad request errors for images in deployed environment

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,34 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'storage.googleapis.com', // For Strapi Cloud storage
+            '*.strapiapp.com', // For Strapi Cloud domains
+            'https:', // Allow all HTTPS images as fallback
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'storage.googleapis.com',
+            '*.strapiapp.com',
+            'https:',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
